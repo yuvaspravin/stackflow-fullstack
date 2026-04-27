0a0f16ed9933f9fd15bb338ba43bc22f7daae330
yuvas pravin <yuvaspravin.sage@outlook.com>
2026-04-27 20:44:56 +0530

    base url change

--- client/src/redux/api/apiSlice.js
+++ client/src/redux/api/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const apiSlice = createApi({
   reducerPath: "api",
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000/api" }),
+  baseQuery: fetchBaseQuery({ baseUrl: "import.meta.env.VITE_API_URL" }),
   // Registered all domain tags for automatic cache invalidation
   tagTypes: ["Product", "User", "Order", "Customer"],
 
