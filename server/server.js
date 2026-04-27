cb2a50d485ad2d52665b896d447deed5e5977ded
yuvas pravin <yuvaspravin.sage@outlook.com>
2026-04-27 20:39:57 +0530

    server file change

--- server/server.js
+++ server/server.js
@@ -4,8 +4,16 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 
 const app = express();
-app.use(cors());
+
 app.use(express.json());
+
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  }),
+);
 
 // Connect to MongoDB with Auto-Heal armor
 mongoose
